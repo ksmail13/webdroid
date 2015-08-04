dbdd31673a47fefdf9a5f17e05531fbb06234d3e
YoonjuChoi <h2yoonjub2@gmail.com>
2015-08-04 17:17:28 +0900

    8.4
    setting-page

--- 아카이브/settingtest2.js
+++ 아카이브/settingtest2.js
@@ -1,19 +1,23 @@
-(function($){ //테스트
+//**************************Modal-alert****************************************//
+(function($){ //modal alert
+    $.fn.modal_opts = [];
 	$.fn.my_modal_box = function(prop){
 
-		var options = $.extend(prop);
-       		
+        $.fn.modal_opts[this.attr('id')] = $.extend(true, {}, $.extend(prop));
+        
 		return this.click(function(e){
+            var id = $(this).attr('id');
+            var opt = $.fn.modal_opts[$(this).attr('id')];
 			add_block_page();
-			add_popup_box();
+			add_popup_box(opt);
 		});
 		 
 		 function add_block_page(){
-			var block_page = $('<div class="modal_block_page screen-center"></div>');						
+			var block_page = $('<div class="out"><div class="modal_block_page screen-center"></div></div>');						
 			$(block_page).appendTo('body');
 		}
 		 		
-		 function add_popup_box(){
+		 function add_popup_box(options){
              var myModal=$('<div class="modal-dialog"><div class="modal-content"></div></div>');
              $(myModal).appendTo('.modal_block_page');
                       
@@ -22,23 +26,24 @@
                  myModal_header += '<h4 class="modal-title">'+options.title +'</h4></div>';
              
              var myModal_body='<div class="modal-body">';
+             /*
                  myModal_body +='<div class="form-group">';
                  myModal_body +='<label class="sr-only" for="txt_in_user_id">'+options.label+'</label>';
                  myModal_body +='<input type="'+options.type+'" name="'+options.name;
                  myModal_body +='" class="form-control input-lg" id="txt_in_user_id" placeholder="'+options.placeholder+'">'
                  myModal_body +='</div>';
-                 
+               */  
              
-            /* for(var i=0; i<contents.length;i++) {
-        var content = contents[i];
-        myModal_body +='<div class="form-group">'
-        myModal_body+='<label class="sr-only" for="txt_in_user_id">'+content.label+'</label>';
-        myModal_body+='<input type="'+content.type+' name="'+content.name+'" class="form-control input-lg" id="txt_in_user_id"                    placeholder="'+content.placeholder+'">';
-        myModal_body+='</div>';
-    }*/
+                for(var i=0; i<options.contents.length;i++) {
+                    var content = options.contents[i];
+                    myModal_body +='<div class="form-group">'
+                    myModal_body+='<label class="sr-only" for="txt_in_user_id">'+content.label+'</label>';
+                    myModal_body+='<input type="'+content.type+'" name="'+content.name+'" class="form-control input-lg" id="txt_in_user_id"                    placeholder="'+content.placeholder+'">';
+                    myModal_body+='</div>';
+                }
              
                  myModal_body +='<div class="form-group">';
-                 myModal_body +='<input type="button" class="setting_btn padding5 '+options.btnclass+' " value="'+options.button+'">';
+                 myModal_body +='<input type="button" class="setting_btn padding5 '+options.btnclass+'" value="'+options.button+'">';
                  myModal_body += '</div>';
                  myModal_body +='</div>';
 
@@ -48,6 +53,13 @@
              $('.close').click(function(){
                  $('.modal-dialog').remove();
              });
+            
+             /*
+             $('.out').click(function(){
+                 $('.modal-dialog').remove();
+             });
+             */
+             
              
              //////////////git등록버튼/////////////////////
              $('.gitsubmit').click(function(){
@@ -59,115 +71,96 @@
                     }
                 });
                  $('.modal-dialog').remove();
-                 window.layerAlert('깃허브아이디 등록','등록되었습니다.');
+                 window.layerAlert('GIT hub 아이디 등록','등록되었습니다.','gitok','확인');
              });
              
              /////////////pw변경 버튼/////////////////////////
-              function validatePassword(){ 
-                 var validator = $("#pw-form").validate({
-                    rules: {"new-password" :"required",
-                            "confirm-new-password":{qualTo: "#new-password"}
+             function validatePassword(){ 
+                 var validator = $(".modal-body").validate({
+                    rules: {"new-pw" :"required",
+                            "new-pw-confirm":{qualTo: "#new-pw"}
                             },                             
                     });
                  if(validator.form()){
                     // ajax
                  }
-              }
+             }
                   
              $('.pwsubmit').click(function(){
+                   
+                if(validatePassword()==true){
                     $('.modal-dialog').remove();
-                    validatePassword();                    
-                    window.layerAlert(' 비밀번호 변경','변경되었습니다');
-                    return false;
+                    window.layerAlert(' 비밀번호 변경','변경되었습니다','pwok','확인');
+                }
+                else{}
+                return false;
+            });
+           
+             ////////////회원탈퇴//////////////////////////
+            $('.members-pw-submit').click(function(){
+                
+                $.ajax({
+                    'type':'POST',
+                    'url': "#",
+                    'success': function(result){
+                    $("#div1").html(result);  //이부분 
+                    }
                 });
+                 $('.modal-dialog').remove();
+                 window.layerAlert('탈퇴하기','탈퇴되었습니다.','unsubscribeok','확인');
+             });
              
-             ////////////////////////////////////////////
+             /////////////////////////////////////////////
 		}
 		return this;
 	};
 
 })(jQuery);
 
-	window.layerAlert = function(title,msg,callback) {
+//**************************확인alert****************************************//
+window.layerAlert = function(title,msg,btnclass, btnvalue, callback) {
     
-    var myHTML  = '<div class="out"> <div id="dv-alert" class="dialog screen-center">';
-        myHTML += '<div class="dialog-bg">';
-        myHTML += '<div class="dialog-title">'+title;
-        myHTML += '<button type="button" class="close-modal alert-close">'
-        myHTML += '<span aria-hidden="true">&times;</span>';
-        myHTML += '</button>';
+    var myHTML ='<div class="modal_alert_page screen-center">';
+        myHTML += '<div class="modal-dialog"><div class="modal-content">';
+    
+        myHTML += '<div class="modal-header">';
+        myHTML += '<button type="button" class="close" data-dismiss="modal" >&times;</button>'
+        myHTML += '<h4 class="modal-title">'+title +'</h4>';
         myHTML += '</div>';
-        myHTML += '<div class="dialog-msg">'+msg+'</div>';
-        myHTML += '<div class="dialog-buttons">';
-        myHTML += '<button onclick=""  class="large green button alert-close">확 인</button>';
+    
+        myHTML += '<div class="modal-body">';
+        myHTML += '<p align="center">'+msg+'</p>';
+        myHTML += '<div class="form-group">';
+        myHTML += '<input type="button" class="setting_btn padding5 '+btnclass+ '"  value="'+btnvalue+ '">';
         myHTML += '</div>';
         myHTML += '</div>';
-        myHTML += '</div></div>';
         
-    
-    $('.modal .close').click();
+        myHTML += '</div>';
+        myHTML += '</div>';
+        myHTML += '</div>';
+        
     $('body').append(myHTML);
-    $('.alert-close').click(function(event) {
-        $('#dv-alert').remove();
-        if(callback != null && callback != undefined)
-            callback();
-    });
     
-     $('.out').click(function(event){
-        $('#dv-alert').remove();
+    ///////////////////회원탈퇴//////////////////////
+   
+    $('.members-submit').my_modal_box({
+            title:'탈퇴를 위한 비밀번호 확인',
+            contents:[{label:'confirmPW', name:'confrim-pw', type:'password', placeholder:"비밀번호"}],
+            btnclass:'members-pw-submit',                  
+            button:'탈퇴하기'
+    });
+    //////////////////////////////////////////////
+    
+    $('.'+btnclass).click(function(){
+        if(btnclass!='members-submit'){
+            $('.modal-dialog').remove();
+            if(callback != null && callback != undefined)
+                callback();
+            }
+        });
+    
+    
+     $('.close').click(function(event){
+        $('.modal-dialog').remove();
     });
 };
-
-
-
-//github 아이디 등록 
-$(document).ready(function(){
-    
-    $("#gitsubmit").click(function(){
-        $.ajax({
-            'type':'POST',
-            'url': "#",
-            'success': function(result){
-            $("#div1").html(result);  //이부분 
-        }});
-        window.layerAlert('깃허브아이디 등록','등록되었습니다.');
-        
-       
-    });
-});
-
-// 비밀번호 변경 
-function validatePassword(){ 
- var validator = $("#pw-form").validate({
-  rules: {                   
-   "new-password" :"required",
-   "confirm-new-password":{
-    equalTo: "#new-password"
-      }  
-     },                             
- });
-    
-    
- if(validator.form()){
-    // ajax
- }
-    window.layerAlert(' 비밀번호 변경','변경되었습니다');
-    return false;
-}
-
-//회원탈퇴
-$(document).ready(function(){
-    $("#unsubscribe-submit").click(function(){
-        $.ajax({
-            'type':'POST',
-            'url': "#",
-            'success': function(result){
-            $("#div1").html(result);  //이부분
-                
-        }});
-        
-        window.layerAlert('회원 탈퇴 하기','탈퇴 되었습니다.');
-         
-    });
-});
-
