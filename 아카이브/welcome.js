7da55fa3f7c74bd03dfa23086ecaa686dcf28320
YoonjuChoi <h2yoonjub2@gmail.com>
2015-08-10 15:40:18 +0900

    8.10
    welcome

---
+++ 아카이브/welcome.js
@@ -0,0 +1,20 @@
+ $('#loginbtn').click( function() {
+                var loginData{
+                    user-id: $("#user-id").val(),
+                    user-pw: $("user-pw").val()
+                };
+                $.ajax({
+                    url:'/signin',
+                    dataType:'json',
+                    type:'POST',
+                    data: loginData,
+                    success:function(result){
+                        if(result==true){
+                            alert("로그인 성공");
+                        }
+                        else{
+                            alert("실패");
+                        }
+                    }
+                });
+            });
