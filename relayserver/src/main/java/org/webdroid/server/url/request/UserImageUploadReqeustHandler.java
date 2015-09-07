4c4dbe4ea877699f0c273ec399f1276a3cab6864
Micky Kim <ksmail13@gmail.com>
2015-09-07 18:44:07 +0900

    server
    server

--- relayserver/src/main/java/org/webdroid/server/url/request/UserImageUploadReqeustHandler.java
+++ relayserver/src/main/java/org/webdroid/server/url/request/UserImageUploadReqeustHandler.java
@@ -1,5 +1,6 @@
 package org.webdroid.server.url.request;
 
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.sql.UpdateResult;
@@ -7,11 +8,13 @@ import io.vertx.ext.web.FileUpload;
 import org.webdroid.constant.HttpStatusCode;
 import org.webdroid.constant.Query;
 import org.webdroid.constant.ResultMessage;
+import org.webdroid.constant.WebdroidConstant;
 import org.webdroid.db.DBConnector;
 import org.webdroid.db.SQLResultHandler;
 import org.webdroid.server.handler.RouteHandler;
 import org.webdroid.util.JsonUtil;
 
+import java.util.Calendar;
 import java.util.Set;
 
 /**
@@ -20,19 +23,30 @@ import java.util.Set;
 public class UserImageUploadReqeustHandler extends RouteHandler {
 
     public static final String URL = "/p_img_upload";
-    DBConnector mDBConnector = null;
+    private DBConnector mDBConnector = null;
 
+    private Vertx vertx = null;
 
+    FileUpload upload;
     String path;
 
-    public UserImageUploadReqeustHandler(DBConnector dbConnector) {
+    public UserImageUploadReqeustHandler(DBConnector dbConnector, Vertx vertx) {
         mDBConnector = dbConnector;
+        this.vertx = vertx;
     }
 
     @Override
     public void handling() {
         Set<FileUpload> uploads = context.fileUploads();
-        uploads.stream().forEach(upload -> path = upload.uploadedFileName());
+        uploads.stream().forEach(upload -> this.upload = upload);
+        Calendar c = Calendar.getInstance();
+
+
+        String pImageName = String.format("%d_profile_%ld.%s",
+                (Integer)session.get("id"), c.getTimeInMillis(), upload.fileName());
+        logger.debug(String.format("file save in %s", pImageName));
+        vertx.fileSystem().copyBlocking(upload.uploadedFileName(), WebdroidConstant.Path.UPLOAD_IMG+"/"+pImageName);
+
         JsonArray dbParms = JsonUtil.createJsonArray(path, session.get("id"));
         mDBConnector.update(Query.IMG_UPLOAD, dbParms, new SQLResultHandler<UpdateResult>(this) {
             @Override
