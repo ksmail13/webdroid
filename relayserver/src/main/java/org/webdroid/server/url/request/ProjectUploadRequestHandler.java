98579b3a1c7ff8af41fa081e228d985f1fae3598
Micky Kim <ksmail13@gmail.com>
2015-09-10 21:17:34 +0900

    file upload request
    request

---
+++ relayserver/src/main/java/org/webdroid/server/url/request/ProjectUploadRequestHandler.java
@@ -0,0 +1,28 @@
+package org.webdroid.server.url.request;
+
+import io.vertx.ext.web.FileUpload;
+import org.webdroid.constant.HttpStatusCode;
+import org.webdroid.db.DBConnector;
+import org.webdroid.server.handler.RequestHandler;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by micky on 2015. 9. 10..
+ */
+public class ProjectUploadRequestHandler extends RequestHandler {
+    public ProjectUploadRequestHandler(DBConnector dbConnector) {
+        super(dbConnector, true, "p_name");
+    }
+
+    @Override
+    public void handlingWithParams(Map<String, Object> params) {
+        Set<FileUpload> uploads = context.fileUploads();
+
+        logger.debug("uploads");
+        uploads.forEach(upload -> logger.debug(upload.fileName() + " " + upload.uploadedFileName() + " " + upload.name()));
+
+        sendJsonResult(HttpStatusCode.SUCCESS, false, "not implement");
+    }
+}
