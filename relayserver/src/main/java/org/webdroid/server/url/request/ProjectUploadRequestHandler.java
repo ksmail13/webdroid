fdd5221a21858de518a0b09444aa98d9d6a7d86e
SehoAhn <sekong14@hanmail.net>
2015-09-18 04:44:40 +0900

    unzipped project path DB update

--- relayserver/src/main/java/org/webdroid/server/url/request/ProjectUploadRequestHandler.java
+++ relayserver/src/main/java/org/webdroid/server/url/request/ProjectUploadRequestHandler.java
@@ -1,12 +1,17 @@
 package org.webdroid.server.url.request;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.UpdateResult;
 import io.vertx.ext.web.FileUpload;
 import org.webdroid.constant.HttpStatusCode;
+import org.webdroid.constant.Query;
 import org.webdroid.constant.ResultMessage;
 import org.webdroid.constant.WebdroidConstant;
 import org.webdroid.db.DBConnector;
+import org.webdroid.db.SQLResultHandler;
 import org.webdroid.server.handler.RequestHandler;
 import org.webdroid.util.JsonUtil;
 import org.webdroid.util.UnZipper;
@@ -22,6 +27,8 @@ public class ProjectUploadRequestHandler extends RequestHandler {
     public static final String URL = "/fileup";
     private Vertx vertx = null;
     FileUpload upload;
+    Set<FileUpload> uploads;
+    private int pid;
 
     public ProjectUploadRequestHandler(DBConnector dbConnector, Vertx vertx) {
         super(dbConnector, true, "p_name", "p_descript");
@@ -31,30 +38,64 @@ public class ProjectUploadRequestHandler extends RequestHandler {
 
     @Override
     public void handlingWithParams(Map<String, Object> params) {
-        Set<FileUpload> uploads = context.fileUploads();
+        uploads = context.fileUploads();
         if(uploads.size() > 0) {
-            uploads.stream().forEach(upload -> this.upload = upload);
-
-            System.out.println(upload.fileName());
-            String zipFileName = String.format("%d_%s_%s", 1, req.getParam("p_name"), upload.fileName());
-            String unzipPathFolderName = String.format("%d_%s", 1, req.getParam("p_name"));
-
-            String zipFilePath = WebdroidConstant.Path.UPLOAD_PROJECT + "/" + zipFileName;
-            logger.debug(String.format("project file save in %s", zipFilePath));
-            logger.debug(upload);
-
-            vertx.fileSystem().copy(upload.uploadedFileName(), zipFilePath, voidAsyncResult -> {
-                UnZipper.unZipper(zipFileName, unzipPathFolderName);
+            JsonArray dbParams = JsonUtil.createJsonArray(params.get("p_name"), params.get("p_descript"), session.get("id"));
+            mDBConnector.update(Query.NEW_PROJECT, dbParams, new SQLResultHandler<UpdateResult>(this) {
+                @Override
+                public void success(UpdateResult result) {
+                    getPid();
+                }
             });
-            logger.debug("uploads");
-            uploads.forEach(upload -> logger.debug(upload.fileName() + " " + upload.uploadedFileName() + " " + upload.name()));
-
-            //sendJsonResult(HttpStatusCode.SUCCESS, false, "not implement");
-            JsonObject res = JsonUtil.createJsonResult(true, ResultMessage.SUCCESS);
-            send(HttpStatusCode.SUCCESS, "application/json", res.toString());
-
         } else {
             sendJsonResult(HttpStatusCode.SUCCESS, false, "no file upload");
         }
+    }
+
+    public void getPid(){
+        mDBConnector.query("select LAST_INSERT_ID();", null,new SQLResultHandler<ResultSet>(this){
+            @Override
+            public void success(ResultSet resultSet) {
+                pid = resultSet.getResults().get(0).getInteger(0);
+                //System.out.println(pid);
+                makeProjectPath();
+            }
+        });
+    }
+
+    public void makeProjectPath(){
+        uploads.stream().forEach(upload -> this.upload = upload);
+
+        System.out.println(upload.fileName());
+        String zipFileName = String.format("%d_%s_%s", pid, req.getParam("p_name"), upload.fileName());
+        String unzipPathFolderName = String.format("%d_%s", pid, req.getParam("p_name"));
+
+        String zipFilePath = WebdroidConstant.Path.UPLOAD_PROJECT + "/" + zipFileName;
+        String unzipPath = WebdroidConstant.Path.UPLOAD_PROJECT + "/" + unzipPathFolderName + "/";
+        logger.debug(String.format("project file save in %s", zipFilePath));
+        logger.debug(upload);
+
+        vertx.fileSystem().copy(upload.uploadedFileName(), zipFilePath, voidAsyncResult -> {
+            UnZipper.unZipper(zipFileName, unzipPathFolderName);
+        });
+        logger.debug("uploads");
+        uploads.forEach(upload -> logger.debug(upload.fileName() + " " + upload.uploadedFileName() + " " + upload.name()));
+
+        JsonArray dbParams = JsonUtil.createJsonArray(session.get("id"),pid);
+        mDBConnector.update(Query.UPDATE_USER_PROJECT, dbParams, new SQLResultHandler<UpdateResult>(this) {
+            @Override
+            public void success(UpdateResult result) {
+            }
+        });
+        dbParams = JsonUtil.createJsonArray(unzipPath, pid);
+        mDBConnector.update(Query.SET_PATH, dbParams, new SQLResultHandler<UpdateResult>(this) {
+            @Override
+            public void success(UpdateResult result) {
+            }
+        });
+
+        //sendJsonResult(HttpStatusCode.SUCCESS, false, "not implement");
+        JsonObject res = JsonUtil.createJsonResult(true, ResultMessage.SUCCESS);
+        send(HttpStatusCode.SUCCESS, "application/json", res.toString());
     }
 }
